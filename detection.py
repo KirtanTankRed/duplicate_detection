5a2aab9f258564f205bab7eda50e2d8128b1786c
KirtanTankRed <168075756+KirtanTankRed@users.noreply.github.com>
2024-07-29 15:20:58 +0530

    Update detection.py

--- detection.py
+++ detection.py
@@ -241,14 +241,6 @@ def auto_suggest_best_image(duplicate_groups, img_dir):
                     grayscale_image = ImageOps.grayscale(image)
                     grayscale_image.save(img_path)
                     displayed_images[img_name].image(grayscale_image, use_column_width=True)
-            # Redisplay the images in the group
-            for idx, img_name in enumerate(group):
-                img_path = os.path.join(img_dir, img_name)
-                image = Image.open(img_path)
-                if img_name == best_image_name:
-                    cols[idx].image(image, use_column_width=True, caption="Best Image")
-                else:
-                    cols[idx].image(image, use_column_width=True)
 
 # Function to clear the image folder (In-memory this case)
 def clear_img_folder(img_dir):
@@ -298,4 +290,3 @@ if 'duplicate_groups' in st.session_state:
         display_images_for_deletion(st.session_state['duplicate_groups'], img_dir)
     elif option == "Auto Suggestion":
         auto_suggest_best_image(st.session_state['duplicate_groups'], img_dir)
-
